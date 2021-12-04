e4f5bc6eaca9a22bfc5a8e642ccfc094b32cd6ae
gauri <gauri.1910@gmail.com>
2021-12-03 17:02:38 -0800

    initial commit

---
+++ src/main/java/com/gauri/pizzahouse/PizzaHouseApplication.java
@@ -0,0 +1,13 @@
+package com.gauri.pizzahouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gauri/pizzahouse/PizzaHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauri.pizzahouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
